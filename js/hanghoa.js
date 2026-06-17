444cd54647d5aeaf5fc09634892242c699081008
phong <hanhphong48@gmail.com>
2026-06-17 19:35:41 +0700

    update 1706

--- js/hanghoa.js
+++ js/hanghoa.js
@@ -1,61 +1,63 @@
-const products ={
+const products = [
      
 {
     name: "123",
     price: 10000,
-    description: "ghi chú vào",
-    link: "chitiet.html",
-    Image:"../assets/images/tailoc.jpg#"
+    ttin: "ghi chú vào",
+    link: "#",
+    Image:"../assets/images/tailoc.jpg"
 },
  
 {
-  name: "123",
+    name: "123",
     price: 10000,
-    description: "ghi chú vào",
-    link: "chitiet.html",
-    Image:"../assets/images/tailoc.jpg#"  
+    ttin: "ghi chú vào",
+    link: "#",
+    Image:"../assets/images/tailoc.jpg"  
 },
 {
     name: "123",
     price: 10000,
-    description: "ghi chú vào",
-    link: "chitiet.html",
-    Image:"#../assets/images/tailoc.jpg"
+    ttin: "ghi chú vào",
+    link: "#",
+    Image:"../assets/images/tailoc.jpg"
 },
 
 {
     name: "14443",
     price: 10000,
-    description: "ghi chú vào",
-    link: "chitiet.html",
-    Image:"#../assets/images/tailoc.jpg"
+    ttin: "ghi chú vào",
+    link: "#",
+    Image:"../assets/images/tailoc.jpg"
 }
-}
+];
+
 
 function createItem(obj)
 {
-    const productlist = document.getElementById("product-list")
-    productlist.innerHTML +=
-    `   <div class ="col text-center product-item">
-            <div class ="card ">
-                 <div class ="ratio ratio-1x1 product-image">
-                   <img src="${obj.Image}" alt="${obj.name}" class="card-img-top">   
-                </div>
-            <div class ="card-body product-info">
-                  <h4 class ="card-title text-danger">${obj.name}</h4>   
-                   <h5 class ="text-danger">${obj.price}</h5>
-                    <p style="text-align: justify;"> ${description}</p>
-                     <a href="${obj.link}" class ="btn btn-info">xemmmmm chi tiết</a>
-                </div>
-            </div>
-          </div>
-    `
+const list = document.getElementById("product-list") ;
+list.innerHTML +=`
+<div class=" col ">
+    <div class="card product-item" >
+        <div class = "product-image">
+            <img class="card-img-top" src="${obj.Image}" alt="${obj.name}">
+        </div><div class="card-body bg-light product-info">
+             <h4 class = "card-title">${obj.name}</h4>
+            <h5 class = "card-text">${obj.price}</h5>
+            <p class = "card-text" >${obj.ttin}</p>
+            <a class = "btn btn-info" href = "${obj.link}">Xem chi tiết</a>
+         </div>
+    </div>
+</div>
+`;
 }
 
-function loadAllProducts(objArray)
+
+
+function loadAll (objArray)
 {
     let i = 0;
-    while(i<objArray.Length)
+    while(i<objArray.length)
  {
     createItem(objArray[i]);
     i++;
@@ -101,3 +103,23 @@ function loadAllProducts(objArray)
     
 </body>
 </html> */
+
+/*function createItem(obj)
+{
+    const productlist = document.getElementById("product-list")
+    productlist.innerHTML +=
+    `   <div class ="col text-center product-item">
+            <div class ="card ">
+                 <div class ="ratio ratio-1x1 product-image">
+                   <img src="${obj.Image}" alt="${obj.name}" class="card-img-top">   
+                </div>
+            <div class ="card-body product-info">
+                  <h4 class ="card-title text-danger">${obj.name}</h4>   
+                   <h5 class ="text-danger">${obj.price}</h5>
+                    <p style="text-align: justify;"> ${obj.ttin}</p>
+                     <a href="${obj.link}" class ="btn btn-info">xemmmmm chi tiết</a>
+                </div>
+            </div>
+          </div>
+    `
+}*/
---
+++ js/info.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+        name: "sting",
+        price: "10.000đ",
+        image: "../assets/images/tailoc.jpg",
+        info: "uong qua da",
+        link: "#"
+    },
+    {
+        name: "sting",
+        price: "10.000đ",
+        image: "../assets/images/tailoc.jpg",
+        info: "uong sieu da",
+        link: "#"
+    },
+    {
+        name: "sting",
+        price: "10.000đ",
+        image: "../assets/images/tailoc.jpg",
+        info: "uong sieu da",
+        link: "#"
+    },
+    {
+        name: "sting",
+        price: "10.000đ",
+        image: "../assets/images/tailoc.jpg",
+        info: "uong sieu da",
+        link: "#"
+    }
+]
+
+function createItem(obj) 
+{
+    const productList = document.getElementById("product-list")
+    productList.innerHTML += `
+    <div class ="col product-item">
+				<div class="card ">
+					<div class=" ratio ratio-1x1 product-image">
+						<img src= "${obj.image}" alt="${obj.name}" class="card-img-top object-fit-cover">
+					</div>
+					<div class="card-body product-info">
+						<h4 class="card title">${obj.name}</h4>
+						<h6 class="text-danger">${obj.price}</h6>
+						<p>${obj.info}</p>
+						<a href="${obj.link}" class="btn btn-primary">Còn hàng</a>
+					</div>
+	            </div>
+    </div>`;
+}
+
+function AllItem (objArray) {
+    let i = 0;
+    while (i<objArray.length) {
+        createItem(objArray[i]);
+        i++;
+    }
+} 
